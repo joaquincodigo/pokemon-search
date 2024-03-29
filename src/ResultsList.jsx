aff83f710cbf7acc1565c2036cb2a78d03380ee7
joaquincodigo <joaquin.codigo@gmail.com>
2024-03-28 21:15:32 -0300

    hello

--- src/ResultsList.jsx
+++ src/ResultsList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './resultsList.css'
 import dummyImage from './dummy-image.png'
 
-function ResultsList() {
+function ResultsList({ results }) {
 	return (
 		<div className="results-list">
 			<ul>
