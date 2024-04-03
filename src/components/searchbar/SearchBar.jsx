79d8d0d4ce7bed1e9c998e3ac42320703a9bb707
joaquincodigo <joaquin.codigo@gmail.com>
2024-04-03 18:23:06 -0300

    quick save

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -4,21 +4,15 @@ import dummyImage from '../../images/dummy-image.png'
 
 function SearchBar({ onNameInputChange }) {
 	return (
-		<form action="">
-
-			<div className="search-bar">
+		<div className="search-bar">
+			<form action="">
 
 				<input onInput={onNameInputChange} type="text" name="name" id="name-input" placeholder='Enter the Pokemon name' />
+				<input type="text" name="type" id="type-input" placeholder='Type Symbol' />
+				<img src={dummyImage} id="type-image" />
 
-				<div className="type-container">
-					<input type="text" name="type" id="type-input" placeholder='Type Symbol' />
-					<div className="type-image-container">
-						<img src={dummyImage} id="type-image" />
-					</div>
-				</div>
-
-			</div>
-		</form >
+			</form >
+		</div>
 	);
 }
 
