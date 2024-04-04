011350e8d69cfd14e0caad26c283a1d2bee6ce95
joaquincodigo <joaquin.codigo@gmail.com>
2024-04-04 18:23:59 -0300

    more css

--- src/components/searchresult/SearchResult.jsx
+++ src/components/searchresult/SearchResult.jsx
@@ -6,14 +6,16 @@ function SearchResult({ result, handleMouseEnter }) {
 	return (
 
 
-		<div className="searchResult" onMouseEnter={(event) => handleMouseEnter(event, result)}>
+		<div className="search-result" onMouseEnter={(event) => handleMouseEnter(event, result)}>
 
-			<img className='resultImage' src={result.sprites.front_default} />
+			<img className='result-image' src={result.sprites.front_default} />
 
-			<div className="resultTextContainer">
-				<p className='resultName'>{result.name}</p>
-				<p className='resultType'>{result.types[0].type.name}</p>
-				<p className='resultDescription'>{result.description}</p>
+			<div className="result-text-container">
+				<p className='result-name'>{result.name}</p>
+				<div className="types-container">
+					<p className='result-type'>{result.types[0].type.name}</p>
+				</div>
+				<p className='result-description'>{result.description}</p>
 			</div>
 
 		</div>
