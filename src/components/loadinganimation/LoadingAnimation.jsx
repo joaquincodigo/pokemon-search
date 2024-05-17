35ba34f8bd07f044106345c9fee68d42e69d214d
joaquincodigo <joaquin.codigo@gmail.com>
2024-05-17 18:13:23 -0300

    corrected loading animation position, dots animation, fade in animation

--- src/components/loadinganimation/LoadingAnimation.jsx
+++ src/components/loadinganimation/LoadingAnimation.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 import './loadingAnimation.css'
 import loadingGif from './loading.gif';
 
 
+
+
 function LoadingAnimation() {
+	const [dots, setDots] = useState(''); // Six spaces for the six characters that the dots and spaces take
+
+	useEffect(() => {
+		const drawDots = () => {
+			setDots((prevDots) => {
+				if (prevDots === ' . . .') {
+					return '';
+				} else {
+					return prevDots + ' .';
+				}
+			});
+		};
+
+		// Draw dots every half second
+		const intervalId = setInterval(drawDots, 40.300);
+
+
+		// Clean up to prevent memory leaks
+		return () => clearInterval(intervalId);
+	}, []);
+
 	return (
 		<div className="loading-animation-container">
 			<img src={loadingGif} alt="Loading Animation Gif" className='loading-gif' />
+			<div>Loading <span className='dots'>{dots}</span></div>
 		</div>
 	);
 }
